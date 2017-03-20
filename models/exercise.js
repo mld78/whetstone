dce5901bb0fded426823a8a4bd72d523b76bf793
James Essex <JMEssex@gmail.com>
2017-03-20 16:50:01 -0700

    tabbing issues

--- models/exercise.js
+++ models/exercise.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-	Test = require('./test')
+		Test = require('./test')
 
 // it's possible the name "method" isn't the best, we could
 // consider changing it to function or something.
