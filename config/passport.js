d924b508098110f0811503d1f2b6fa5aa41a07ce
Brian Schleifer <brianschleifer@gmail.com>
2017-03-21 15:25:26 -0700

    Adjusted passport and User file

--- config/passport.js
+++ config/passport.js
@@ -19,24 +19,28 @@ module.exports = function(passport) {
 
 
 passport.use('local-signup', new LocalStrategy({
+	titlenameField : 'title',
+	nameField : 'name',
 	usernameField : 'email',
 	passwordField : 'password',
 	passReqToCallback : true
-}, function(req, email, password, callback) {
+}, function(req, title, name, email, password, callback) {
 	 process.nextTick(function() {
-
 		 // Find a user with this e-mail
 		 User.findOne({ 'local.email' :  email }, function(err, user) {
 			 if (err) return callback(err)
 
 			 // If there already is a user with this email
 			 if (user) {
+
 				 return callback(null, false, req.flash('signupMessage', 'This email is already used.'))
 			 } else {
 			 // There is no email registered with this email
 
 				 // Create a new user
 				 var newUser            = new User()
+				 newUser.local.title    = title
+				 newUser.local.name    = name
 				 newUser.local.email    = email
 				 newUser.local.password = newUser.encrypt(password)
 
