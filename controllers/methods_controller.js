8e436f7ece1319576f23e2669c3b183eba3f56b2
Gus Nordhielm <gnordhielm@gmail.com>
2017-03-23 17:17:38 -0700

    added more API routes

--- controllers/methods_controller.js
+++ controllers/methods_controller.js
@@ -1,4 +1,5 @@
-var Method = require('../models/method')
+var Method = require('../models/method'),
+    Exercise = require('../models/exercise')
 
 ////////// INTERFACE ACTIONS //////////
 
@@ -12,14 +13,12 @@ function index(req, res) {
   })
 }
 
-
 // NEW
 function newMethod(req, res) {
   res.render(`./admin/method_form.ejs`, {
     message: req.flash(`adminMessage`)
   })
 }
-
 
 // CREATE
 function createMethod(req, res) {
@@ -29,7 +28,6 @@ function createMethod(req, res) {
     res.redirect('/methods/'+ method.id)
   })
 }
-
 
 // SHOW
 function show(req, res) {
@@ -46,7 +44,6 @@ function show(req, res) {
   })
 }
 
-
 // EDIT
 function editMethod(req, res) {
   var slug = req.params.slug_url
@@ -59,7 +56,6 @@ function editMethod(req, res) {
     })
   })
 }
-
 
 // UPDATE
 function updateMethod(req, res) {
@@ -83,43 +79,70 @@ function updateMethod(req, res) {
   })
 }
 
-
 // DELETE
 function destroyMethod(req, res) {
   var id = req.params.id
-  // var areYouSure = prompt(`ARE YOU SURE YOU WANT TO DELETE THIS METHOD?\nType: "YES DELETE METHOD"\n\n${req.body}`)
-  // if (areYouSure === "YES DELETE METHOD") {
-
   Method.remove({_id: id}, function(err) {
     if (err) res.json({message: `Could not delete Method b/c: ${err}`})
 
     res.json({message: 'Method successfully deleted.'});
   })
-
-  // }
 }
 
 ////////// API ACTIONS //////////
 
-// function indexJSON (req, res){
+function indexJSON (req, res){
+  Method.find({}, function(err, methods) {
+    if (err) throw err
+    res.json(methods)
+  })
+}
 
-// }
+function showJSON (req, res){
+  Method.findById(req.params.id, function(err, method) {
+    if (err || !method) {
+      res.json({message: 'No such method.'})
+    } else {
+    res.json(method)
+    }
+  })
+}
 
-// function showJSON (req, res){
+function createJSON (req, res){
+  var newMethod = new Method(req.body)
+  newMethod.save(function(err, method) {
+    if (err || !method) {
+      res.json({message: 'Could not create method.'})
+    } else{
+      res.json(method)
+    }
+  })
+}
 
-// }
+function updateJSON(req, res) {
+  Method.findById(req.params.id, function(err, updatedMethod) {
+    if (err || !updatedMethod) res.json({message: 'No such method.'})
 
-// function createJSON (req, res){
+    if (req.body.name) updatedMethod.name = req.body.name
+    if (req.body.language) updatedMethod.language = req.body.language
+    if (req.body.description) updatedMethod.description = req.body.description
+    if (req.body.version_added) updatedMethod.version_added = req.body.version_added
+    if (req.body.docs_url) updatedMethod.docs_url = req.body.docs_url
+    if (req.body.slug_url) updatedMethod.slug_url = req.body.slug_url
 
-// }
+    updatedMethod.save(function(err, method) {
+      if (err) res.json({message: 'Update method failed.'})
+      res.json(method)
+    })
+  })
+}
 
-// function updateJSON (req, res){
-
-// }
-
-// function destroyJSON (req, res){
-
-// }
+function destroyJSON (req, res){
+  Method.remove({_id: req.params.id}, function(err) {
+    if (err) res.json({message: 'Unable to delete method.'})
+    res.json({message: 'Method deleted.'})
+  })
+}
 
 // EXPORTS
 module.exports = {
@@ -131,9 +154,9 @@ module.exports = {
   updateMethod: updateMethod,
   destroyMethod: destroyMethod,
 
-  // indexJSON: indexJSON,  
-  // showJSON: showJSON,  
-  // createJSON: createJSON,  
-  // updateJSON: updateJSON,  
-  // destroyJSON: destroyJSON
+  indexJSON: indexJSON,  
+  showJSON: showJSON,  
+  createJSON: createJSON,  
+  updateJSON: updateJSON,  
+  destroyJSON: destroyJSON
 }
